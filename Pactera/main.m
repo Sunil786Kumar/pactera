6f20a7022a7e6b88bed8e5184e50dd6be82f7c6f
Sunil Kumar <sunil.786.kumar@gmail.com>
2015-02-06 16:13:58 +1100

    Initial Commit

---
+++ Pactera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pactera
+//
+//  Created by Sunil Kumar on 6/02/2015.
+//  Copyright (c) 2015 Sunil Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ PacteraTests/PacteraTests.m
@@ -0,0 +1,40 @@
+//
+//  PacteraTests.m
+//  PacteraTests
+//
+//  Created by Sunil Kumar on 6/02/2015.
+//  Copyright (c) 2015 Sunil Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PacteraTests : XCTestCase
+
+@end
+
+@implementation PacteraTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
